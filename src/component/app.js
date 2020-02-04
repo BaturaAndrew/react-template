6f7e64357b2ff31611253ad37ae579dfd5a71f90
Batura Andrew <baturaandrew@gmainl.com>
2020-02-04 15:57:03 +0300

    init

---
+++ src/component/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./app.scss";
+class MyComponent extends Component {
+  render() {
+    return <div className="intro">Hello World</div>;
+  }
+}
+export default MyComponent;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "./main.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(), 
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'file-loader',
+        type: 'javascript/auto',
+        options: {
+          name() {
+            return '[name].[ext]';
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+};
